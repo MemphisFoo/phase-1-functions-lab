71256d8045032e941c8dd35305452f66373509f9
Christian Trublet de Nermont <cjwtdn@gmail.com>
2022-07-03 16:43:37 -0600

    commit tree 5ac6a31fd71eee7c0df5d6f5445ad815b735a07e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const hq = 42;
+const block = 264;
+
+function distanceFromHqInBlocks(location) {
+  if(location > hq) { 
+    return location - hq;}
+    else{return hq - location;}
+};
+
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * block;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if(destination > start) { 
+    return (destination - start) * block;}
+    else{return (start - destination) * block};
+}
+
+function calculatesFarePrice(start, destination) {
+  const result = distanceTravelledInFeet(start, destination);
+  if (result <= 400) {
+    return 0}
+  else if (result <= 2000) {
+    return (result - 400) * .02}
+  else if (result < 2500) {return 25}
+else{ return "cannot travel that far"} 
+}
